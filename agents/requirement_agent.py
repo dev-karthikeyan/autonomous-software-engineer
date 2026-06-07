5b506751fb318686b2d2f03e2592dd55ef703ca8
KARTHIK-65-DARK <keyankarthikeyan622@gmail.com>
2026-06-07 18:22:21 +0530

    Make Requirement Agent accept dynamic user input

---
+++ agents/requirement_agent.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+llm=ChatMistralAI(model="mistral-small-2603")
+
+def requirement_agent(user_request:str) :
+
+    prompt = f"""
+
+    You are a Senior Software Requirements Analyst.
+
+    Analyze the user request and extract:
+
+    1. Project Type
+    2. Features
+    3. Technologies
+    4. Expected Output
+
+    user_request :
+                 {user_request}
+
+"""
+    
+    responce=llm.invoke([HumanMessage(content=prompt)])
+
+    return responce.content 
+
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from agents.requirement_agent import requirement_agent
+
+user_request = input("ENTER YOUR PROJECT REQUIREMENT :")
+
+result=requirement_agent(user_request)
+
+print(result)
